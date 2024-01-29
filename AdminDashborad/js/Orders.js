5a83d871cf308de39f740ee3b8e5a13f39e7783e
Yousef <mohamedyousefhoutar@gmail.com>
2024-01-29 21:11:59 +0200

    add some update

--- AdminDashborad/js/Orders.js
+++ AdminDashborad/js/Orders.js
@@ -11,35 +11,40 @@ else{
 
 console.log(ordersData);
 
+// read data
+function showData() {
+  let table = '';
+  
+  if (ordersData.length > 0) {
+    for (let i = 0; i < ordersData.length; i++) {
+      let count = ''; // Initialize count as an empty string for each iteration
 
-//read data
-function showData(){
+      // Assuming 'quantity' is an array of objects with 'numOfItems' property
+      for (let j = 0; j < ordersData[i]['quantity'].length; j++) {
+        count += ordersData[i]['quantity'][j].numOfItems;
 
-    let table='';
-    for(let i=0;i<ordersData.length;i++){
-      
-        table += `
+        // Add a comma 
+        if (j < ordersData[i]['quantity'].length - 1) {
+          count += ', ';
+        }
+      }
+
+      table += `
         <tr>
           <td>${(ordersData[i]['Order Id'] * 100).toFixed(0)}</td>
           <td>${ordersData[i]['productId']}</td>
           <td>${ordersData[i]['First name']} ${ordersData[i]['Last name']}</td>
-          <td>${ordersData[i]['quantity'][0].numOfItems},${ordersData[i]['quantity'][1].numOfItems}</td>
+          <td>${count}</td>
           <td>${ordersData[i]['products total price ']}</td>
           <td>${ordersData[i]['Date of Order']}</td>
         </tr>`;
-      }
-      //console.log(table);
+    }
+  } else {
+    table = "<tr><td colspan='6'>Not found Orders</td></tr>";
+  }
 
-      if(ordersData.length<=0){
-        table="<p>not found Orders</p>";
-      }
-
-
-
-    document.getElementById("tbody").innerHTML = table;
-
+  document.getElementById("tbody").innerHTML = table;
 }
 
-    //display data
-    showData();
-
+// display data
+showData();
